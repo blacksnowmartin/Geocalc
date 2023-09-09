a0298d31f1b68745838ebcd256c19bb74039db44
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2023-09-09 13:09:14 +0300

    Update Calculate_area_from_kml_file.py
    A tip on to ensure the code works.

--- Calculate_area_from_kml_file.py
+++ Calculate_area_from_kml_file.py
@@ -9,3 +9,5 @@ area_sq_meters = gdf.geometry.area.sum()
 area_sq_km = area_sq_meters / 1e6  # Convert square meters to square kilometers
 
 print(f"Approximate Area: {area_sq_km:.2f} square kilometers")
+
+# BEFORE YOU START THE PROGRAM ENSURE YOU RUN THIS CODE AT BASH pip install geopandas
